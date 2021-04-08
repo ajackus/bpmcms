54a3f3bcd188171070415fed13582c71ee36fb7d
Tanveer Khan <tanveer.khan@ajackus.com>
2021-04-08 10:43:20 +0530

    Added new plugin to send an email

--- config/plugins.js
+++ config/plugins.js
@@ -10,4 +10,21 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    provider: 'nodemailer',
+    providerOptions: {
+      host: env('SMTP_HOST', 'smtp.example.com'),
+      port: env('SMTP_PORT', 587),
+      secure: true,
+      auth: {
+        user: env('SMTP_USERNAME'),
+        pass: env('SMTP_PASSWORD'),
+      },
+      // ... any custom nodemailer options
+    },
+    settings: {
+      defaultFrom: env('MAIL_FROM'),
+      defaultReplyTo: env('MAIL_REPLYTO'),
+    },
+  },
 });
